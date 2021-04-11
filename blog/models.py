574cd232029968730cf6ec8ed8e3760eca286f10
EngineeringDisasters <scottcohen10@gmail.com>
2021-04-11 12:22:35 -0400

    Setups admin, next connect bootstrap templates to static

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Blog_post(models.Model):
+    title = models.CharField(max_length=100)
+    date_posted = models.DateField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    content = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='blog/images/')
+
+    def __str__(self):
+        return self.title
