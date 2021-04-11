8aae37dff835d96a85fe3c1ad3a9c541c9ae5a23
EngineeringDisasters <scottcohen10@gmail.com>
2021-04-11 11:48:47 -0400

    Continued setup including bootstrap static attempt

--- scott_cohen_portfolio/settings.py
+++ scott_cohen_portfolio/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'general',
+    'projects',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT =  BASE_DIR / 'media'
